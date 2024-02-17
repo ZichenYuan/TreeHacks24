86a3bb834106e050cf9b48fb9441bd4e52c6b29f
ZichenYuan <y.zichen@wustl.edu>
2024-02-17 00:16:21 -0800

    Initial Commit

---
+++ 3d-bereal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3d-bereal
+//
+//  Created by Zichen Yuan on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
